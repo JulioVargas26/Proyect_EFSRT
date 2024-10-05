d25671103ea49b00e41d8fe2cadf018f85f69e31
JulioVargas26 <84108359+JulioVargas26@users.noreply.github.com>
2024-10-05 18:37:18 -0500

    revision

---
+++ src/main/java/com/proyect/util/TipoMoneda.java
@@ -0,0 +1,13 @@
+package com.proyect.util;
+
+public enum TipoMoneda {
+    USD_DOLAR_AMERICANO,
+    EUR_EURO,
+    PEN_SOL_PERUANO,
+    JPY_YEN_JAPONES,
+    CNY_YUAN_CHINO,
+    MXN_PESO_MEXICANO,
+    VEF_BOLIVAR,
+    BRL_REAL_BRASILERO,
+    RUB_RUBLO_RUSO
+}
